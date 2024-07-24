ad5761ec39421a32ed185e27b0cdbf0beb5e1e4b
franciscogrimes <fgsieg1831@gmail.com>
2024-07-24 11:12:29 -0300

    Configuração do ambiente e conectando ao DB

---
+++ src/server.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const routes = require("./routes/routes")
+const connection = require("./database/connection")
+const APP_PORT = process.env.APP_PORT
+
+class Server {
+    
+    constructor(server = express()) {
+        this.middlewares(server)
+        this.database()
+        server.use(routes)
+        this.initializeServer(server)
+    }
+
+    async middlewares(server) {
+        console.log("Executando os middlewares")
+        server.use(express.json())
+        console.log("Middlewares executados")
+    }
+
+    async database() {
+        try {
+            console.log("Conectando ao banco de dados")
+            await connection.authenticate()
+        } catch (error) {
+            console.log("Erro ao conectar ao banco de dados: ", error)
+        }
+    }
+
+    async initializeServer(server) {
+        server.listen(APP_PORT, () => {
+            console.log(`Servidor rodando na porta ${APP_PORT}!`)
+        })
+    }
+}
+
+module.exports = { Server }
