433308b30bd36469bd7c50e40617187bee5ab8fe
franciscogrimes <fgsieg1831@gmail.com>
2024-07-25 16:15:11 -0300

    Adicionando tratativas para o erro 409

--- src/controllers/UsuariosControllers.js
+++ src/controllers/UsuariosControllers.js
@@ -7,14 +7,26 @@ class UsuariosControllers {
             const dados = request.body
 
             if(!dados.nome || !dados.email || !dados.password){
-                return response.status(400).json({mensagem: "Preencha todos os campos obrigatórios."})
+                return response.status(400).json({mensagem: "Preencha todos os campos obrigatórios. (Nome, Email e Senha)"})
             }
 
-            const usuario = await Usuario.create(dados)
+            const emailExistente = await Usuario.findOne({ where: { email: dados.email } });
+            const cpfExistente = await Usuario.findOne({ where: { cpf: dados.cpf } });
 
-            return response.status(201).json(`${usuario.nome} registrado com sucesso!`);
+
+            if (emailExistente) {
+                return response.status(409).json({ mensagem: "Email já cadastrado." });
+            } else if (cpfExistente) {
+                return response.status(409).json({ mensagem: "CPF já cadastrado." });
+            } else {
+                const usuario = await Usuario.create(dados);
+                return response.status(201).json(`${usuario.nome} registrado com sucesso!`);
+            }
+
+          
 
         }catch(error){
+            console.log(error)
             return response.status(500).json({mensagem : "Não foi possível criar o usuário, devido a um erro interno"})
         }
     }
