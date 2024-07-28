411f7c7d9815f88763d96606e565c34c1895f5ff
franciscogrimes <fgsieg1831@gmail.com>
2024-07-28 15:05:54 -0300

    Implementação do valida token

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,15 @@
 const { Router } = require('express')
 const usuariosRoutes = require('./usuarios.routes')
 const loginRoutes = require('./login.routes')
+const validaToken = require('../middlewares/auth')
 
 
 const routes = new Router()
 
 routes.use('/usuarios', usuariosRoutes)
 routes.use('/login', loginRoutes)
+
+routes.use(validaToken)
+
 
 module.exports = routes
