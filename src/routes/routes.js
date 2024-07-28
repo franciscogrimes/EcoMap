ca64e401a817b9a65c1fa86c15179092488db09e
franciscogrimes <fgsieg1831@gmail.com>
2024-07-28 15:45:58 -0300

    Validação de autenticador e login

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,16 +2,17 @@ const { Router } = require('express')
 const usuariosRoutes = require('./usuarios.routes')
 const loginRoutes = require('./login.routes')
 const pontosRoutes = require('./pontos.routes')
-const validaToken = require('../middlewares/auth')
+const auth = require('../middlewares/auth')
 
 
 const routes = new Router()
 
 routes.use('/usuarios', usuariosRoutes)
 routes.use('/login', loginRoutes)
-routes.use('/local', pontosRoutes)
 
-routes.use(validaToken)
+routes.use(auth)
+routes.use('/local',auth, pontosRoutes)
+
 
 
 module.exports = routes
