c60831f0d5818071183924fe7001bc8e16005a91
franciscogrimes <fgsieg1831@gmail.com>
2024-07-28 01:39:44 -0300

    Adicionando o link do google maps

--- src/services/service.js
+++ src/services/service.js
@@ -23,4 +23,18 @@ async function getMapLocal(cep){
     }
 }
 
-module.exports = getMapLocal
+async function getGoogleMapsLink(local){
+    try {
+        const { lat, lon } = local;
+
+        const googleMapsLink = `https://www.google.com/maps?q=${lat},${lon}`;
+
+        return googleMapsLink;
+
+    } catch(error){
+        console.error(error);
+        throw new Error('Erro gerar o link do Google Maps');
+    }
+}
+
+module.exports = getMapLocal, getGoogleMapsLink
