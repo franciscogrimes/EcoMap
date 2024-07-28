eb111a0c032dfd1536483fb437e3e86d6e7d10c2
franciscogrimes <fgsieg1831@gmail.com>
2024-07-28 01:01:57 -0300

    Adicionando a rota de pontos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
 const usuariosRoutes = require('./usuarios.routes')
 const loginRoutes = require('./login.routes')
+const pontosRoutes = require('./pontos.routes')
 
 
 const routes = new Router()
 
 routes.use('/usuarios', usuariosRoutes)
 routes.use('/login', loginRoutes)
+routes.use('/pontos', pontosRoutes)
 
 module.exports = routes
